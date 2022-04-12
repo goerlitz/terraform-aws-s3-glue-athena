d385c1f704309507394d430e99355615cef122b0
Olaf Görlitz <olaf.goerlitz@gmail.com>
2022-04-12 09:00:30 +0200

    using axios in download, minor improvements

--- src/data-api/download.ts
+++ src/data-api/download.ts
@@ -1,60 +1,71 @@
 // inspired by
 // https://futurestud.io/tutorials/node-js-how-to-download-a-file
-// test with: node -e "console.log(require('./download').handler({url: 'https://raw.githubusercontent.com/tblock/10kGNAD/master/test.csv', filename: 'gnad/test/test.csv'}));"
+// test with:
+// node -e 'console.log(require("./dist/data-api/download").handler({httpMethod: "POST", body: "{\"url\": \"https://raw.githubusercontent.com/tblock/10kGNAD/master/test.csv\", \"filename\": \"test.csv\"}"}));'
+// curl -X POST -H "Content-Type: application/json" -d '{"url": "https://raw.githubusercon/10kGNAD/master/test.csv", "filename": "test.csv"}' "$(terraform output -raw base_url)/download"
 
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult
-} from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import * as fs from "fs";
+import axios from "axios";
 
-import { IncomingMessage } from 'http'
-import * as https from 'https'
-import * as fs from 'fs'
+interface RequestParams {
+  url: string;
+  filename: string;
+  s3_bucket: string;
+  s3_path: string;
+}
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  console.log("got request:", event);
 
-    console.log('got request:', event)
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: `"${event.httpMethod}" method not allowed`,
+    };
+  }
 
-    const url = `https://raw.githubusercontent.com/tblock/10kGNAD/master/test.csv`;
-    const url2 = new URL(url);
-    console.log('filename:', url2.pathname.split('/').pop());
+  const params = JSON.parse(event.body) as RequestParams;
 
-    let message = "";
+  try {
+    // wait for request to finish
+    const res = await axios.get(params.url, { responseType: "stream" });
+    console.log("statusCode:", res.status);
 
-    const result = await new Promise<string>((resolve, reject) => {
-      https.get(url, (res: IncomingMessage) => {
+    // extract filename and filesize
+    const content_length = res.headers["content-length"];
+    console.log("content-length", content_length);
 
-        console.log('statusCode:', res.statusCode);
-        console.log('headers:', res.headers);
+    // open file in local filesystem
+    const file = fs.createWriteStream(params.filename);
 
-        // extract filename and filesize
+    // stream data from response to local file
+    res.data.pipe(file);
 
-        const content_length = res.headers["content-length"];
-        console.log('content-length', content_length);
-
-        // Open file in local filesystem
-        const file = fs.createWriteStream(`test.csv`);
-
-        // Write data into local file
-        res.pipe(file);
-
-        // Close the file
-        file.on('finish', () => {
-          file.close();
-          console.log(`File downloaded!`);
-        });
-
-        resolve(JSON.stringify(res.headers));
-
-      }).on("error", (err) => {
-        reject(`{err.message}`)
-      });
+    // Close the file
+    file.on("finish", () => {
+      file.close();
+      console.log(`File downloaded!`);
     });
+
+    // upload file to s3
+    // https://docs.aws.amazon.com/code-samples/latest/catalog/javascript-s3-s3_upload.js.html
 
     return {
       statusCode: 200,
-      body: result,
+      body: JSON.stringify(res.headers),
     };
-}
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(error);
+    } else {
+      console.log(error);
+    }
+    return {
+      statusCode: 500,
+      body: error,
+    };
+  }
+};
