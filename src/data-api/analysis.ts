89d72ee76fe8ed298b9204b5f03a7ad2506a2edc
Olaf Görlitz <olaf.goerlitz@gmail.com>
2022-04-14 21:26:51 +0200

    linted analysis script

--- src/data-api/analysis.ts
+++ src/data-api/analysis.ts
@@ -1,41 +1,43 @@
-import { AthenaExpress, ConnectionConfigInterface } from "athena-express";
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import * as AWS from "aws-sdk";
+import { AthenaExpress, ConnectionConfigInterface } from 'athena-express';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
 
 const config: Partial<ConnectionConfigInterface> = {
   aws: AWS,
-  s3: "s3://tft-athena-query-results/output",
-  db: "datasets_db",
-  workgroup: "tft_athena_workgroup",
+  s3: 's3://tft-athena-query-results/output',
+  db: 'datasets_db',
+  workgroup: 'tft_athena_workgroup',
   getStats: true,
 };
 
 const athenaExpress = new AthenaExpress(config);
 
 export const handler = async (
-  event: APIGatewayProxyEvent
+  event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
-  console.log("request:", event);
+  console.log('request:', event);
 
-  const sqlQuery = `SELECT * FROM gnad_train WHERE label = 'Sport' LIMIT 2;`;
+  const sqlQuery = 'SELECT * FROM gnad_train WHERE label = \'Sport\' LIMIT 2;';
 
   try {
     const results = await athenaExpress.query(sqlQuery);
-    console.log("results:", results);
+    console.log('results:', results);
     return {
       statusCode: 200,
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         message: results.Items,
       }),
     };
   } catch (error) {
-    console.log("error", error);
+    console.log('error', error);
     return {
       statusCode: 500,
-      body: JSON.stringify("Execution failed! " + error),
+      body: JSON.stringify('Execution failed! ') + error,
     };
   }
 };
+
+export default handler;
